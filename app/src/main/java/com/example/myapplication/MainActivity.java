1daad05294fce4b58e589654e73cb0e5e4e9928f
Raymond <raymond@nusantarabetastudio.com>
2022-01-04 06:11:42 +0700

    [ADD] session 11

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.myapplication;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    int value;
+    TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.countTextView);
+        findViewById(R.id.startButton).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.startButton) {
+            MyAsyncTask asyncTask = new MyAsyncTask();
+            asyncTask.execute(10);
+        }
+    }
+
+    class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(MainActivity.this, "mulai", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            textView.setText(values[0] + "");
+        }
+
+        @Override
+        protected String doInBackground(Integer... params) {
+            value = params[0];
+            while (value >= 0) {
+                publishProgress(value);
+                value--;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return "Selesai";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
